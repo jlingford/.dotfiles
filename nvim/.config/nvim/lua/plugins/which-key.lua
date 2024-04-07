d5823c8307b4f257582d0edfe7c4c5f22d54a8be
jlingford <james.lingford@gmail.com>
2024-04-07 22:52:03 +1000

    fix: which-key prefix

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -1,13 +1,41 @@
+local wk = require("which-key")
+
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
-  opts = {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+    end,
+    opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
-  }
+        wk.register({
+            ["<leader>f"] = {
+                name = "+find (Telescope)",
+            },
+            ["<leader>t"] = {
+                name = "+terminal",
+            },
+            ["<leader>gh"] = {
+                name = "+git hunk",
+            },
+            ["<leader>b"] = {
+                name = "+buffer",
+            },
+            ["<leader>w"] = {
+                name = "+windows",
+            },
+            ["<leader>c"] = {
+                name = "+code",
+            },
+            ["<leader>s"] = {
+                name = "+Spectre",
+            },
+            ["<leader>x"] = {
+                name = "+Trouble",
+            },
+        })
+    },
 }
