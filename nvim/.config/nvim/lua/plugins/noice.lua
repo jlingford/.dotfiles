71b94fc238a09097f30b3b057c559b74aa18b9af
jlingford <james.lingford@gmail.com>
2024-05-29 21:54:40 +1000

    fix(nvim): noice.nvim shows macro recording notification

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -2,50 +2,47 @@ return {
     "folke/noice.nvim",
     event = "VeryLazy",
     dependencies = {
-        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
         "MunifTanjim/nui.nvim",
-        -- OPTIONAL:
-        --   `nvim-notify` is only needed, if you want to use the notification view.
-        --   If not available, we use `mini` as the fallback
         "rcarriga/nvim-notify",
     },
-    config = function()
-        require("noice").setup({
-            lsp = {
-                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-                override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                    ["cmp.entry.get_documentation"] = true,
-                },
+    opts = {
+        lsp = {
+            -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true,
             },
-            presets = {
-                -- bottom_search = true, -- use a classic bottom cmdline for search
-                -- command_palette = false, -- position the cmdline and popupmenu together
-                -- long_message_to_split = true, -- long messages will be sent to a split
-                -- inc_rename = false, -- enables an input dialog for inc-rename.nvim
-                -- lsp_doc_border = false, -- add a border to hover docs and signature help
-            },
-            update_in_insert = false,
-            routes = {
+        },
+        presets = {
+            bottom_search = true, -- use a classic bottom cmdline for search
+            command_palette = false, -- position the cmdline and popupmenu together
+            long_message_to_split = true, -- long messages will be sent to a split
+            inc_rename = false, -- enables an input dialog for inc-rename.nvim
+            lsp_doc_border = true, -- add a border to hover docs and signature help
+        },
+        update_in_insert = false,
+        routes = {
+            {
+                view = "notify",
                 filter = {
-                    event = "msg_show",
-                    kind = "",
-                },
-                opts = { skip = true },
-            },
-            cmdline = {
-                view = "cmdline",
-                format = {
-                    search_down = {
-                        view = "cmdline",
-                    },
-                    search_up = {
-                        view = "cmdline",
+                    -- min_width = 500,
+                    event = "msg_showmode",
+                    any = {
+                        { find = "recording" },
                     },
                 },
             },
-        })
-    end,
-    opts = {},
+        },
+    },
+    keys = {
+        { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
+        { "<leader>il", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
+        { "<leader>ih", function() require("noice").cmd("history") end, desc = "Noice History" },
+        { "<leader>ia", function() require("noice").cmd("all") end, desc = "Noice All" },
+        { "<leader>id", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
+        { "<leader>it", function() require("noice").cmd("telescope") end, desc = "Noice Telescope" },
+        { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll Forward", mode = {"i", "n", "s"} },
+        { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll Backward", mode = {"i", "n", "s"}},
+    },
 }
