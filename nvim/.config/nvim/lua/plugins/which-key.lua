399ddedda52517e578902de627d0e69e7b49c858
jlingford <james.lingford@gmail.com>
2024-08-25 19:25:26 +1000

    fix(nvim): fix which-key to work with latest version

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -1,46 +1,32 @@
 return {
 	"folke/which-key.nvim",
-	keys = { "<leader>" },
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 300
-	end,
+	event = "VeryLazy",
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
+	opts_extend = { "spec" },
 	opts = {
 		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
 		plugins = { spelling = true },
-		defaults = {
-			-- old mappings
+		defaults = {},
+		spec = {
 			mode = { "n", "v" },
-			["<leader>f"] = { name = "+find (Telescope)" },
-			["<leader>t"] = { name = "+terminal" },
-			["<leader>g"] = { name = "+git" },
-			["<leader>gh"] = { name = "+git hunks" },
-			["<leader>b"] = { name = "+buffer" },
-			["<leader>w"] = { name = "+windows" },
-			["<leader>c"] = { name = "+code" },
-			["<leader>s"] = { name = "+Spectre" },
-			["<leader>x"] = { name = "+Trouble" },
-			["<leader>i"] = { name = "+Noice" },
+			{ "<leader>f", group = "find (Telescope)" },
+			{ "<leader>t", group = "terminal" },
+			{ "<leader>g", group = "git" },
+			{ "<leader>gh", group = "git hunks" },
+			{ "<leader>b", group = "buffer" },
+			{ "<leader>w", group = "windows" },
+			{ "<leader>c", group = "code" },
+			{ "<leader>s", group = "Spectre" },
+			{ "<leader>x", group = "Trouble" },
+			{ "<leader>i", group = "Noice" },
 		},
-		-- new mappings?
-		-- 	mode = { "n", "v" },
-		-- 	{ "<leader>b", group = "buffer" },
-		-- 	{ "<leader>c", group = "code" },
-		-- 	{ "<leader>f", group = "find (Telescope)" },
-		-- 	{ "<leader>g", group = "git" },
-		-- 	{ "<leader>gh", group = "git hunks" },
-		-- 	{ "<leader>i", group = "Noice" },
-		-- 	{ "<leader>s", group = "Spectre" },
-		-- 	{ "<leader>t", group = "terminal" },
-		-- 	{ "<leader>w", group = "windows" },
-		-- 	{ "<leader>x", group = "Trouble" },
-		-- },
 	},
-	config = function(_, opts)
-		local wk = require("which-key")
-		wk.setup(opts)
-		wk.register(opts.defaults)
-	end,
 }
