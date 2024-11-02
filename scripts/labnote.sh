a3e966a1bcbe430a33a8ca14a978988aa10daacb
jlingford <james.lingford@gmail.com>
2024-11-02 11:47:52 +1100

    update labnote.sh script

--- scripts/labnote.sh
+++ scripts/labnote.sh
@@ -1,14 +1,33 @@
 #!/usr/bin/env bash
 
 # USAGE:
-# labnote [TITLE]
+# labnote.sh [TITLE]
 
+# take first command line argument
 TITLE=$1
 
-touch "$TITLE"_$(date -u +%Y%m%d).md
+# find file with highest num prefix in current dir, return just the num
+HIGHEST_NUM=$(fd -d1 "^[0-9]+" | tail -n 1 | sed -E 's/^([0-9]+)_.*/\1/')
+
+# if no previous file, set number prefix to 00
+if [[ -z $HIGHEST_NUM ]]; then
+    HIGHEST_NUM="00"
+fi
+
+# increment for next number
+NEXT_NUM=$((HIGHEST_NUM + 1))
+
+# set new prefix for new file (requires printf for leading zeros)
+FILE_PREFIX=$(printf "%02d" ${NEXT_NUM})
+
+# set filename
+FILENAME="${FILE_PREFIX}_${TITLE}_$(date -u +%Y%m%d).md"
+
+# create new file
+touch ${FILENAME}
 
 echo "---
-title: '$TITLE'
+title: '${TITLE}'
 author: 'James Lingford'
 date: '$(date -u +%Y-%m-%d)'
 toc: true
@@ -19,6 +38,6 @@ format:
         html-math-method: katex
 ---
 
-# $TITLE
+# ${TITLE}
 
-" >>"$TITLE"_$(date -u +%Y%m%d).md
+" >>${FILENAME}
