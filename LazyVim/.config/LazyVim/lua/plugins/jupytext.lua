5005e0997ede5760833bf73a83293bdd38dad7f1
jlingford <james.lingford@gmail.com>
2025-07-11 23:41:48 +1000

    fix inline docs

--- LazyVim/.config/LazyVim/lua/plugins/jupytext.lua
+++ LazyVim/.config/LazyVim/lua/plugins/jupytext.lua
@@ -2,16 +2,8 @@ return {
   "GCBallesteros/jupytext.nvim",
   config = true,
   -- WARN: do not try lazy loading, just won't work...
-  -- Depending on your nvim distro or config you may need to make the loading not lazy
-  -- lazy=false,
-  -- WARN: requires a venv with "pip install jupytext" ran
-  -- require("jupytext").setup({
-  --   style = "markdown",
-  --   output_extension = "md",
-  --   force_ft = "markdown",
-  -- }),
-  -- event = "VeryLazy",
-  -- ft = { "markdown", "quarto", "ipynb", "json" },
+  lazy = false,
+  -- WARN: requires a venv with "pip install jupytext" already installed!!
   opts = {
     style = "markdown",
     output_extension = "md",
