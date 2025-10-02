65bbe35122e0c010bd483b793f4ccc08d52b7524
jlingford <james.lingford@gmail.com>
2025-10-02 17:05:52 +1000

    BIG lazyvim update!

--- LazyVim/.config/LazyVim/lua/plugins/colors.lua
+++ LazyVim/.config/LazyVim/lua/plugins/colors.lua
@@ -132,15 +132,15 @@ return {
       },
     },
     specs = {
-      {
-        "akinsho/bufferline.nvim",
-        optional = true,
-        opts = function(_, opts)
-          if (vim.g.colors_name or ""):find("catppuccin") then
-            opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
-          end
-        end,
-      },
+      -- {
+      --   "akinsho/bufferline.nvim",
+      --   optional = true,
+      --   opts = function(_, opts)
+      --     if (vim.g.colors_name or ""):find("catppuccin") then
+      --       opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+      --     end
+      --   end,
+      -- },
     },
   },
 }
